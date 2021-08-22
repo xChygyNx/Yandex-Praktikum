b50a5a7f547dfcff4efed19156a822a8db3cdbac
urijlifanov <ChykCha112@yandex.ru>
2021-08-22 23:43:43 +0300

    try solve final task a and add description in final task b

--- 4. Hash tables/final_a.py
+++ 4. Hash tables/final_a.py
@@ -1,82 +1,61 @@
-from typing import List, Tuple
+from typing import List, Tuple, Set
 
-A = 107
-M = 1000007
+COUNT_DOCS = 0
 
 
 class SearchIndex:
     def __init__(self):
-        self.docs = []
-        self.requests = []
-        self.read_data()
-        self.hash_docs = []
-        self.hash_requests = []
-        self.relevant_score = []
+        self.docs = {}
 
-    def read_data(self):
-        doc_count = int(input())
-        for _ in range(doc_count):
-            self.docs.append(input().split())
-        req_count = int(input())
-        for _ in range(req_count):
-            self.requests.append(input().split())
+    def hashing_docs(self):
+        doc_word = {}
+        global COUNT_DOCS
+        COUNT_DOCS = int(input())
+        for i in range(COUNT_DOCS):
+            doc = [x for x in input().split()]
+            for word in doc:
+                tmp = doc_word.get(word, {})
+                tmp[i] = tmp.get(i, 0) + 1
+                doc_word[word] = tmp
+        self.docs = doc_word
 
-    def hash_word(self) :
-        for phrase in self.docs:
-            line = []
-            for word in phrase:
-                _sum = 0
-                for ch in word:
-                    _sum = _sum * A + ord(ch)
-                    _sum %= M
-                line.append(_sum)
-            self.hash_docs.append(line)
-        for phrase in self.requests:
-            line = []
-            for word in phrase:
-                _sum = 0
-                for ch in word:
-                    _sum = _sum * A + ord(ch)
-                    _sum %= M
-                line.append(_sum)
-            self.hash_requests.append(line)
+    def get_request(self, request: Set[str]):
+        request.clear()
+        words = input().split()
+        for word in words:
+            request.add(word)
 
     def analysis(self) -> List[List[Tuple[int, int]]]:
         result = []
-        meet_words = set()
-        for num_request, phrase_request_hash in enumerate(self.hash_requests):
-            request_scores = []
-            for num_doc, phrase_doc_hash in enumerate(self.hash_docs):
+        count_requests = int(input())
+        request = set()
+        for _ in range(count_requests):
+            self.get_request(request)
+            line = []
+            for i in range(COUNT_DOCS):
                 score = 0
-                meet_words.clear()
-                for num_request_word, word_request_hash in enumerate(phrase_request_hash):
-                    if self.requests[num_request][num_request_word] not in meet_words:
-                        meet_words.add(self.requests[num_request][num_request_word])
-                        for num_doc_word, word_doc_hash in enumerate(phrase_doc_hash):
-                            if word_request_hash == word_doc_hash and\
-                                self.requests[num_request][num_request_word] == self.docs[num_doc][num_doc_word]:
-                                score += 1
-                request_scores.append((-1 * score, num_doc+1))
-            request_scores.sort()
-            result.append(request_scores)
+                for word in request:
+                    tmp = self.docs.get(word, {})
+                    score += tmp.get(i, 0)
+                line.append((score * -1, i + 1))
+            line.sort()
+            result.append(line)
         return result
 
 
-def print_relevant_info(relevant_info: List[List[Tuple[int, int]]]) -> None:
-    result = []
-    for line in relevant_info:
-        line = line[: 5]
-        tmp = [str(x[1]) for x in line if x[0] != 0]
-        try:
-            _ = tmp[0]
-            result.append(' '.join(tmp))
-        except IndexError:
-            pass
-    print('\n'.join(result))
+def get_five_docs(relevants: List[Tuple[int, int]]):
+    length = len(relevants)
+    limit = length if length < 5 else 5
+    for i in range(limit):
+        yield relevants[i]
 
+
+def print_relevant_info(relevant_info: List[List[Tuple[int, int]]]) -> None:
+    for line in relevant_info:
+        print(' '.join((str(x[1]) for x in get_five_docs(line) if x[0] != 0)))
 
 if __name__ == '__main__':
     s_ind = SearchIndex()
-    s_ind.hash_word()
+    s_ind.hashing_docs()
     relevant_info = s_ind.analysis()
     print_relevant_info(relevant_info)
