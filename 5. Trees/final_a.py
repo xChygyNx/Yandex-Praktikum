04b1ebe21cbfa0f386ec63f4f95a4dc315fd38da
urijlifanov <ChykCha112@yandex.ru>
2021-09-09 00:40:30 +0300

    add reviewers notations

--- 5. Trees/final_a.py
+++ 5. Trees/final_a.py
@@ -1,24 +1,21 @@
 """
-ID решения: 52610764
+ID решения: 52799721
 
 ---ПРИНЦИП РАБОТЫ---
 
     Для хранения элементов по принципу минимальной кучи создается экземпляр класса
     Heap, в котором прописаны методы добавления и изъятия минимального значения
-    с сохранением структуры кучи. У класса есть 2 атрибута: хранилище, где в определенном
-    порядке хранятся элементы (обычный список) и текущий размер хранилища, чтобы при
-    вставке и изъятии элемента не приходилось заново пересчитывать размер.
+    с сохранением структуры кучи. У класса есть хранилище, где в определенном
+    порядке хранятся элементы (обычный список).
     При вставке элемента инкрементируется размер хранилища, в конец списка
     вставляется элемент и для последнего элемента осуществляется просеивание
     вверх, чтобы он занял соответствующую его величине почицию в куче.
-    При изъятии элемента из кучи, размер уменьшается на единицу, берется первый
+    При изъятии элемента из кучи, сначала проверяем, если в хранилище лежит только
+    один элемент, то просто удаляем его и возвращаем предварительно сохраненный
+    первый элемент. Если больше одного элемента, то берется первый
     элемент из хранилища (там всегда лежит минимальный элемент), на его позицию
     встает последний элемент в хранилище, и для него осуществляется просеивание
-    вниз. Блок try except нужен для извлечения последнего элемента из хранилища,
-    так как сначала происходит забор последнего элемента и потом его на место
-    первого, при заборе последнего элемента получается что роль первого и последнего
-    элемента исполняет один и тот же элемент, и после его удаления из хранилища
-    мы удаляем и мемто куда его следует вставить.
+    вниз.
     Элементы хранятся в виде кортежей, где первый элемент - это количество решенных
     задач помноженное на -1, второй элемент - штраф участника, третий - его имя.
     Хранение элементов в таком виде обусловлено тем, что так их удобно сравнивать
@@ -32,6 +29,7 @@ ID решения: 52610764
     O(1) времени, так как мы знаем его позицию в списке, вставка последнего элемента
     в начало списка еще O(1), и просеивание вниз так же занимает O(h), или
     O(log n).
+    В обоих случаях временная сложность действия составляет O(log n)
 
 ---ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ---
     Дополнительная память нужна только для хранения входящих данны в списке
@@ -42,52 +40,49 @@ from typing import Tuple
 
 class Heap:
     def __init__(self):
-        self.size = 0
-        self.store = [None]
+        self.store = []
 
     def add(self, value: Tuple[int, int, str]):
         self.store.append(value)
-        self.size += 1
         self.sift_up()
 
     def pop(self) -> Tuple[int, int, str]:
-        self.size -= 1
-        answer = self.store[1]
-        try:
-            self.store[1] = self.store.pop()
+        answer = self.store[0]
+        if len(self.store) > 1:
+            self.store[0] = self.store.pop()
             self.sift_down()
-        except IndexError:
-            pass
+        else:
+            self.store.pop()
         return answer
 
     def sift_up(self):
-        idx = self.size
+        idx = len(self.store) - 1
         while True:
-            if idx == 1:
+            if idx == 0:
                 break
-            if self.store[idx] >= self.store[idx // 2]:
+            if self.store[idx] >= self.store[(idx - 1) // 2]:
                 break
-            self.store[idx], self.store[idx // 2] = self.store[idx // 2], self.store[idx]
-            idx //= 2
+            self.store[idx], self.store[(idx - 1) // 2] = self.store[(idx - 1) // 2], self.store[idx]
+            idx = (idx - 1) // 2
 
     def get_idx_min_elem(self, idx) -> int:
-        if self.store[idx * 2] < self.store[idx * 2 + 1]:
-            return idx * 2
-        return idx * 2 + 1
+        if self.store[idx * 2 + 1] < self.store[idx * 2 + 2]:
+            return idx * 2 + 1
+        return idx * 2 + 2
 
     def sift_down(self):
-        idx = 1
+        idx = 0
         while True:
             try:
-                left_child = self.store[idx * 2]
+                left_child = self.store[idx * 2 + 1]
             except IndexError:
                 break
             try:
-                right_child = self.store[idx * 2 + 1]
+                right_child = self.store[idx * 2 + 2]
             except IndexError:
                 right_child = None
             if right_child is None:
-                new_idx = idx * 2
+                new_idx = idx * 2 + 1
             else:
                 new_idx = self.get_idx_min_elem(idx)
             if self.store[idx] <= self.store[new_idx]:
@@ -111,6 +106,6 @@ def construct_heap() -> Heap:
 if __name__ == '__main__':
     min_heap = construct_heap()
     result = []
-    while min_heap.size > 0:
+    while len(min_heap.store) > 0:
         result.append(min_heap.pop()[2])
     print('\n'.join(result))
