5a09ad4a8bc62a3fec20242593c41432ca6f4aec
urijlifanov <ChykCha112@yandex.ru>
2021-09-08 16:03:40 +0300

    complete final task b

--- 5. Trees/final_b.py
+++ 5. Trees/final_b.py
@@ -1,3 +1,37 @@
+"""
+ID решения: 52761524
+
+---ПРИНЦИП РАБОТЫ---
+    Нужно удалить узел из бинарного дерева поиска сохранив его структуру,
+    т.е. чтобы после удаления оно осталось бинарным деревом поиска. Сначала
+    я ищу в дереве ноду, которую надо удалить, при этом возвращаю не только
+    саму удаляемую ноду, но и ее родителя (если поиск ноды не привел к успеху,
+    то просто возвращаю корень исходного дерева без изменений). После этого
+    ищу ноду, которую поставлю на место удаляемой. Для этого если у этой ноды
+    есть левое поддерово, то иду в него и ищу самый правый узел в нем, если есть
+    только правое поддерево, то иду в него и нахожу самого левый узел. Функция
+    поиска узла на замену помимо возврата найденной подходящей ноды так же
+    редактирует ссылку на эту ноду у родителя (ставит туда или детей ноды, которая
+    вщята на замену, или 'зануляет' ее. После этого, чтобы не менять ссылки
+    на узлы в поддереве в котором произошло удаление ноды, я ставлю в ноду, которую
+    надо удалить значение из ноды найденной на замену и просто спускаюсь
+    по дереву вниз и так же меняю значения нод, чтобы сохранить свойства
+    бинарного дерева поиска
+
+---ВРЕМЕННАЯ СЛОЖНОСТЬ---
+    В алгоритме нам требуется O(h) времени чтобы найти элемент, который
+    требуется удалить, где h - высота дерева. Затем так же O(h) времени
+    чтобы найти ноду на замену удаляемой. O(1) на смену у удаляемой ноды
+    значения и O(h) для просеивания вниз от удаленной ноды для восстановления
+    свойств бинарного дерева поиска
+
+---ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ---
+    Дополнительная память нужна только для хранения входящих данных в виде
+    бинарного дерева (связанного списка где каждая нода может иметь максимум
+    одного предка и макмимум двух потомков). Для этого требуется O(n) дополнительной
+    памяти, где n - количество элементов
+"""
+
 # Comment it before submitting
 # class Node:
 #     def __init__(self, left=None, right=None, value=0):
@@ -18,18 +52,30 @@ def find_remove_node(root, key):
     if root.value >= key:
         return find_remove_node(root.left, key)
     else:
-        return find_remove_node(root.righy, key)
+        return find_remove_node(root.right, key)
 
 
-def find_leaf_node(parent, direction, reverse_direction):
-    child_root = getattr(parent, reverse_direction)
-    while getattr(child_root, direction) is not None:
-        parent = child_root
-        child_root = getattr(child_root, direction)
-    if child_root.left is None and child_root.right is None:
-        setattr(parent, reverse_direction, None)
+def find_leaf_node_in_left(parent):
+    child_root = parent.left
+    if child_root.right is None:
+        parent.left = child_root.left
     else:
-        setattr(parent, reverse_direction, getattr(child_root, reverse_direction))
+        while child_root.right is not None:
+            parent = child_root
+            child_root = child_root.right
+        parent.right = child_root.left
+    return child_root
+
+
+def find_leaf_node_in_right(parent):
+    child_root = parent.right
+    if child_root.left is None:
+        parent.right = child_root.right
+    else:
+        while child_root.left is not None:
+            parent = child_root
+            child_root = child_root.left
+        parent.left = child_root.right
     return child_root
 
 
@@ -53,36 +99,40 @@ def remove(root, key):
         return None
     parent_remove_node, remove_node = find_remove_node(root, key)
     if remove_node is None:
-        return None
+        return init_root
     if remove_node.left is not None:
-        leaf_node = find_leaf_node(remove_node, 'right', 'left')
+        leaf_node = find_leaf_node_in_left(remove_node)
+        remove_node.value = leaf_node.value
     elif remove_node.right is not None:
-        leaf_node = find_leaf_node(remove_node, 'left', 'right')
-    try:
-        if parent_remove_node.left == remove_node:
-            parent_remove_node.left = leaf_node
+        leaf_node = find_leaf_node_in_right(remove_node)
+        remove_node.value = leaf_node.value
+    else:
+        if parent_remove_node is None:
+            return None
         else:
-            parent_remove_node.right = leaf_node
-        leaf_node.left, leaf_node.right = remove_node.left, remove_node.right
-        restore_BST(leaf_node)
-    except AttributeError:
-        pass
+            direction = 'left' if parent_remove_node.left == remove_node else 'right'
+            setattr(parent_remove_node, direction, None)
+            return init_root
+    restore_BST(leaf_node)
+
     return init_root
 
 
-
 def test():
-    node1 = Node(None, None, 2)
-    node2 = Node(node1, None, 3)
-    node3 = Node(None, node2, 1)
-    node4 = Node(None, None, 6)
-    node5 = Node(node4, None, 8)
-    node6 = Node(node5, None, 10)
-    node7 = Node(node3, node6, 5)
-    newHead = remove(node7, 10)
-    assert newHead.value == 5
-    assert newHead.right is node5
-    assert newHead.right.value == 8
+    node1 = Node(None, None, 932)
+    node2 = Node(None, node1, 912)
+    node3 = Node(None, None, 822)
+    node4 = Node(node3, node2, 870)
+    node5 = Node(None, None, 701)
+    node6 = Node(node5, node4, 702)
+    node7 = Node(None, None, 266)
+    node8 = Node(None, node7, 191)
+    node9 = Node(node8, None, 298)
+    node10 = Node(node9, node6, 668)
+    newHead = remove(node10, 545)
+    assert newHead.value == 1
+    assert newHead.right is node3
+    assert newHead.right.value == 2
 
 
 if __name__ == '__main__':
